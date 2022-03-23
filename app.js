3221a88db6882c7c594c69f72315b697dd4a1eb4
Mike Beattie <mike@ethernal.org>
2022-03-23 17:34:46 +1300

    Initial commit
    Signed-off-by: Mike Beattie <mike@ethernal.org>

---
+++ app.js
@@ -0,0 +1,245 @@
+/* global $SD */
+$SD.on('connected', conn => connected(conn));
+
+function connected(jsn) {
+    debugLog('Connected Plugin:', jsn);
+
+    $SD.on('com.github.mjbnz.sd-api-request.didReceiveSettings', jsonObj =>
+        action.onDidReceiveSettings(jsonObj)
+    );
+    $SD.on('com.github.mjbnz.sd-api-request.willAppear', jsonObj =>
+        action.onWillAppear(jsonObj)
+    );
+    $SD.on('com.github.mjbnz.sd-api-request.willDisappear', jsonObj =>
+        action.onWillDisappear(jsonObj)
+    );
+    $SD.on('com.github.mjbnz.sd-api-request.keyUp', jsonObj =>
+        action.onKeyUp(jsonObj)
+    );
+}
+
+var action = {
+    type: 'com.github.mjbnz.sd-api-request',
+    cache: {},
+
+    onDidReceiveSettings: function(jsn) {
+        log('onDidReceiveSettings(): ', jsn);
+
+        const settings = jsn.payload.settings;
+        const api_request = this.cache[jsn.context];
+
+        if(!settings || !api_request) return;
+
+        api_request.updateSettings(settings);
+        this.cache[jsn.context] = api_request;
+
+    },
+
+    onWillAppear: function(jsn) {
+        log('onWillAppear(): ', jsn);
+
+        if (!jsn.payload || !jsn.payload.hasOwnProperty('settings')) return;
+
+        const api_request = new APIRequest(jsn);
+        this.cache[jsn.context] = api_request;
+    },
+
+    onWillDisappear: function(jsn) {
+        log('onWillDisappear(): ', jsn);
+
+        let api_request = this.cache[jsn.context];
+
+        if (api_request) {
+            api_request.destroy();
+            delete this.cache[jsn.context];
+        }
+
+    },
+
+    onKeyUp: function(jsn) {
+        log('onKeyUp(): ', jsn);
+
+        const api_request = this.cache[jsn.context];
+
+        if(!api_request)
+            this.onWillAppear(jsn);
+        else
+            api_request.sendRequest();
+
+    }
+
+};
+
+
+function APIRequest(jsonObj) {
+    var settings = jsonObj.payload.settings,
+        context = jsonObj.context,
+        poll_timer = 0,
+        key_state = null;
+
+    function startPeriodicPoll() {
+
+        if(poll_timer !== 0) {
+            window.clearInterval(poll_timer);
+            poll_timer = 0;
+        }
+
+        const frequency = settings.poll_status_frequency || 15;
+
+        poll_timer = setInterval(function() {
+            sendRequest(do_status_poll = true);
+        }, 1000 * frequency);
+    }
+
+    function sendRequest(do_status_poll = false) {
+        if (!settings.request_url) {
+            $SD.api.showAlert(context);
+            return;
+        }
+
+        if (do_status_poll) {
+            if (!Boolean(settings.response_parse) || !Boolean(settings.poll_status)) return;
+            if (!settings.poll_status_url) return;
+        }
+
+        let url    = do_status_poll ? settings.poll_status_url : settings.request_url;
+        let method = settings.request_method || 'GET';
+
+        const opts = {
+            cache: 'no-cache',
+            headers: constructHeaders(),
+            method: method,
+            body: ['GET', 'HEAD'].includes(method)
+                                    ? undefined
+                                    : settings.request_body,
+        };
+
+        log('sendRequest(): URL:', url, 'ARGS:', opts);
+
+        fetch(url, opts)
+            .then((resp) => checkResponseStatus(resp))
+            .then((resp) => updateImage(resp, do_status_poll))
+            .then((resp) => showSuccess(resp, do_status_poll))
+            .catch(err => {
+                $SD.api.showAlert(context);
+                log(err);
+            }
+        );
+
+        if (!do_status_poll)
+            startPeriodicPoll();
+    }
+
+    function constructHeaders() {
+        let default_headers = settings.request_content_type
+                                ? { 'Content-Type': settings.request_content_type }
+                                : {};
+        let input_headers = {};
+
+        if (settings.request_headers) {
+            settings.request_headers.split(/\n/).forEach(h => {
+                if (h.includes(':')) {
+                    const [name, value] = h.split(/: *(.*)/).map(s => {
+                        return s.trim();
+                    });
+
+                    if (name) {
+                        input_headers[name] = value;
+                    }
+                }
+            });
+        }
+
+        return {
+            ...default_headers,
+            ...input_headers
+        }
+    }
+
+    async function checkResponseStatus(resp) {
+        if (!resp) {
+            throw new Error();
+        }
+        if (!resp.ok) {
+            throw new Error(`${resp.status}: ${resp.statusText}\n${await resp.text()}`);
+        }
+        return resp;
+    }
+
+    async function updateImage(resp, do_status_poll) {
+        if (!settings.response_parse || !settings.image_matched || !settings.image_unmatched)
+            return;
+
+        let json, body;
+        var new_key_state = key_state;
+
+        const prefix = (do_status_poll && settings.poll_status && settings.poll_status_parse) ? 'poll_status' : 'response';
+        const field  = Utils.getProp(settings, `${prefix}_parse_field`, undefined);
+        const value  = Utils.getProp(settings, `${prefix}_parse_value`, undefined);
+
+        if (field  !== undefined && value !== undefined) {
+            json = await resp.json();
+            new_key_state = (Utils.getProperty(json, field) == value);
+        } else if (field !== undefined) {
+            json = await resp.json();
+            new_key_state = !(['false', '0', '', 'undefined'].indexOf(String(Utils.getProperty(json, field)).toLowerCase().trim()) + 1);
+        } else if (value !== undefined) {
+            body = await resp.text();
+            new_key_state = body.includes(value);
+        }
+
+        if (new_key_state == key_state) return;
+
+        key_state = new_key_state;
+
+        path = key_state
+                    ? settings.image_matched
+                    : settings.image_unmatched;
+
+        log('updateImage(): FILE:', path, 'JSON:', json, 'BODY:', body);
+
+        Utils.loadImage(path, img => $SD.api.setImage(context, img));
+
+        return resp;
+    }
+
+    function showSuccess(resp, do_status_poll) {
+        if (!do_status_poll && Boolean(settings.enable_success_indicator))
+            $SD.api.showOk(context)
+        return resp;
+    }
+
+    function updateSettings(new_settings) {
+        settings = new_settings;
+        startPeriodicPoll();
+        sendRequest(do_status_poll = true);
+    }
+
+    function destroy() {
+        if(poll_timer !== 0) {
+            window.clearInterval(poll_timer);
+            poll_timer = 0;
+        }
+    }
+
+    startPeriodicPoll();
+    sendRequest(do_status_poll = true);
+
+    return {
+        sendRequest: sendRequest,
+        updateSettings: updateSettings,
+        destroy: destroy
+    };
+}
+
+function log(...msg) {
+    console.log(`[${new Date().toLocaleTimeString('UTC', {hourCycle: 'h23'})}]`, ...msg);
+    //$SD.api.logMessage(msg.map(stringify).join(' '));
+}
+
+function stringify(input) {
+    if (typeof input !== 'object' || input instanceof Error) {
+        return input.toString();
+    }
+    return JSON.stringify(input, null, 2);
+}
---
+++ propertyinspector/index_pi.js
@@ -0,0 +1,398 @@
+/* global addDynamicStyles, $SD, Utils */
+/* eslint-disable no-extra-boolean-cast */
+
+// change this, if you want interactive elements act on any change, or while they're modified
+var onchangeevt = 'oninput'; // 'oninput';
+
+// cache some nodes to improve performance
+let sdpiWrapper = document.querySelector('.sdpi-wrapper');
+
+var settings = {};
+var $localizedStrings = {};
+
+$SD.on('connected', (jsonObj) => {
+    console.log("connected", jsonObj);
+    addDynamicStyles($SD.applicationInfo.colors, 'connectElgatoStreamDeckSocket');
+
+    $SD.api.getSettings();
+
+    /** Localization */
+    if($localizedStrings && Object.keys($localizedStrings).length > 0) {
+        localizeUI();
+    }
+
+});
+
+$SD.on('didReceiveSettings', (jsonObj) => {
+
+    console.log("didReceiveSettings", jsonObj);
+
+    if(jsonObj && jsonObj.payload && jsonObj.payload.settings) {
+
+        settings = jsonObj.payload.settings;
+
+        Object.keys(settings).map(e => {
+            if (e && e != '') {
+                const el = document.querySelector(`#${e}`);
+                console.log(`searching for: #${e}`, 'found:', el);
+                if (el) {
+                    if (el.type === 'textarea') {
+                        el.value = settings[e];
+                        const maxl = el.getAttribute('maxlength');
+                        const labels = document.querySelectorAll(`[for='${el.id}']`);
+                        if (labels.length) {
+                            for (let x of labels) {
+                                x.textContent = maxl ? `${el.value.length}/${maxl}` : `${el.value.length}`;
+                            }
+                        }
+                    } else if (el.type === 'checkbox') {
+                        el.checked = settings[e];
+                    } else if (el.type === 'file') {
+                        document.querySelector(`.sdpi-file-info[for="${el.id}"]`).textContent = trimFileName(settings[e]);
+                    } else {
+                        el.value = settings[e];
+                    }
+                }
+            }
+        });
+
+        showHideSettings();
+    }
+
+    revealSdpiWrapper();
+});
+
+
+$SD.on('sendToPropertyInspector', (jsn) => {
+    const pl = jsn.payload;
+    if (pl.hasOwnProperty('error')) {
+        sdpiWrapper.innerHTML = `<div class="sdpi-item">
+            <details class="message caution">
+            <summary class="${pl.hasOwnProperty('info') ? 'pointer' : ''}">${pl.error}</summary>
+                ${pl.hasOwnProperty('info') ? pl.info : ''}
+            </details>
+        </div>`;
+    }
+});
+
+// eslint-disable-next-line no-unused-vars
+function revealSdpiWrapper () {
+    sdpiWrapper && sdpiWrapper.classList.remove('hidden');
+}
+
+// our method to pass values to the plugin
+function sendValueToPlugin (value, param) {
+    //console.log($SD, $SD.readyState, $SD.actionInfo, $SD.uuid, param, value);
+    if ($SD.connection && ($SD.connection.readyState === 1)) {
+        const json = {
+            'action': $SD.actionInfo['action'],
+            'event': 'sendToPlugin',
+            'context': $SD.uuid,
+            'payload': {
+                [param]: value
+            }
+        };
+        $SD.connection.send(JSON.stringify(json));
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.body.classList.add(navigator.userAgent.includes("Mac") ? 'mac' : 'win');
+    prepareDOMElements(document);
+    $SD.on('localizationLoaded', (language) => {
+        localizeUI();
+    });
+});
+
+/** the beforeunload event is fired, right before the PI will remove all nodes */
+window.addEventListener('beforeunload', function (e) {
+    e.preventDefault();
+    sendValueToPlugin('propertyInspectorWillDisappear', 'property_inspector');
+    // Don't set a returnValue to the event, otherwise Chromium with throw an error.  // e.returnValue = '';
+});
+
+/** CREATE INTERACTIVE HTML-DOM
+ * where elements can be clicked or act on their 'change' event.
+ * Messages are then processed using the 'handleSdpiItemClick' method below.
+ */
+function prepareDOMElements(baseElement) {
+    baseElement = baseElement || document;
+    Array.from(baseElement.querySelectorAll('.sdpi-item-value')).forEach(
+        (el, i) => {
+            const elementsToClick = [
+                'BUTTON',
+                'OL',
+                'UL',
+                'TABLE',
+                'METER',
+                'PROGRESS',
+                'CANVAS'
+            ].includes(el.tagName);
+            const evt = elementsToClick ? 'onclick' : onchangeevt || 'onchange';
+
+            /** Look for <input><span> combinations, where we consider the span as label for the input
+             *  we don't use `labels` for that, because a range could have 2 labels.
+             */
+            const inputGroup = el.querySelectorAll('input + span');
+            if (inputGroup.length === 2) {
+                const offs = inputGroup[0].tagName === 'INPUT' ? 1 : 0;
+                inputGroup[offs].textContent = inputGroup[1 - offs].value;
+                inputGroup[1 - offs]['oninput'] = function() {
+                    inputGroup[offs].textContent = inputGroup[1 - offs].value;
+                };
+            }
+            /** We look for elements which have an 'clickable' attribute
+             *  we use these e.g. on an 'inputGroup' (<span><input type="range"><span>) to adjust the value of
+             *  the corresponding range-control
+             */
+            Array.from(el.querySelectorAll('.clickable')).forEach(
+                (subel, subi) => {
+                    subel['onclick'] = function(e) {
+                        handleSdpiItemChange(e.target, subi);
+                    };
+                }
+            );
+            /** Just in case the found HTML element already has an input or change - event attached, 
+             *  we clone it, and call it in the callback, right before the freshly attached event
+             */
+            const cloneEvt = el[evt];
+            const fn = Utils.debounce(function(e,i) { handleSdpiItemChange(e.target, i);}, 750);
+            el[evt] = function(e) {
+                if (cloneEvt) cloneEvt();
+                fn(e,i);
+            };
+        }
+    );
+
+    /**
+     * You could add a 'label' to a textares, e.g. to show the number of charactes already typed
+     * or contained in the textarea. This helper updates this label for you.
+     */
+    baseElement.querySelectorAll('textarea').forEach((e) => {
+        const maxl = e.getAttribute('maxlength');
+        e.targets = baseElement.querySelectorAll(`[for='${e.id}']`);
+        if (e.targets.length) {
+            let fn = () => {
+                for (let x of e.targets) {
+                    x.textContent = maxl ? `${e.value.length}/${maxl}` : `${e.value.length}`;
+                }
+            };
+            fn();
+            e.onkeyup = fn;
+        }
+    });
+
+    baseElement.querySelectorAll('[data-open-url]').forEach(e => {
+        const value = e.getAttribute('data-open-url');
+        if (value) {
+            e.onclick = () => {
+                let path;
+                if (value.indexOf('http') !== 0) {
+                    path = document.location.href.split('/');
+                    path.pop();
+                    path.push(value.split('/').pop());
+                    path = path.join('/');
+                } else {
+                    path = value;
+                }
+                $SD.api.openUrl($SD.uuid, path);
+            };
+        } else {
+            console.log(`${value} is not a supported url`);
+        }
+    });
+}
+
+function handleSdpiItemChange(e, idx) {
+
+    /** Following items are containers, so we won't handle clicks on them */
+    
+    if (['OL', 'UL', 'TABLE'].includes(e.tagName)) {
+        return;
+    }
+
+    /** SPANS are used inside a control as 'labels'
+     * If a SPAN element calls this function, it has a class of 'clickable' set and is thereby handled as
+     * clickable label.
+     */
+    if (e.tagName === 'SPAN') {
+        const inp = e.parentNode.querySelector('input');
+        var tmpValue;
+        
+        // if there's no attribute set for the span, try to see, if there's a value in the textContent
+        // and use it as value
+        if (!e.hasAttribute('value')) {
+               tmpValue = Number(e.textContent);
+            if (typeof tmpValue === 'number' && tmpValue !== null) {
+                e.setAttribute('value', 0+tmpValue); // this is ugly, but setting a value of 0 on a span doesn't do anything
+                e.value = tmpValue; 
+            }
+        } else {
+            tmpValue = Number(e.getAttribute('value'));
+        }
+        
+        if (inp && tmpValue !== undefined) {
+            inp.value = tmpValue;
+        } else return;
+    }
+
+    const selectedElements = [];
+    const isList = ['LI', 'OL', 'UL', 'DL', 'TD'].includes(e.tagName);
+    const sdpiItem = e.closest('.sdpi-item');
+    const sdpiItemGroup = e.closest('.sdpi-item-group');
+    let sdpiItemChildren = isList
+        ? sdpiItem.querySelectorAll(e.tagName === 'LI' ? 'li' : 'td')
+        : sdpiItem.querySelectorAll('.sdpi-item-child > input');
+
+    if (isList) {
+        const siv = e.closest('.sdpi-item-value');
+        if (!siv.classList.contains('multi-select')) {
+            for (let x of sdpiItemChildren) x.classList.remove('selected');
+        }
+        if (!siv.classList.contains('no-select')) {
+            e.classList.toggle('selected');
+        }
+    }
+  
+    if (sdpiItemChildren.length && ['radio','checkbox'].includes(sdpiItemChildren[0].type)) {
+        e.value = e.checked;
+    }
+    if (sdpiItemGroup && !sdpiItemChildren.length) {
+        for (let x of ['input', 'meter', 'progress']) {
+            sdpiItemChildren = sdpiItemGroup.querySelectorAll(x);
+            if (sdpiItemChildren.length) break;
+        }
+    }
+
+    if (e.selectedIndex !== undefined) {
+        if (e.tagName === 'SELECT') {
+            sdpiItemChildren.forEach((ec, i) => {
+                selectedElements.push({ [ec.id]: ec.value });
+            });
+        }
+        idx = e.selectedIndex;
+    } else {
+        sdpiItemChildren.forEach((ec, i) => {
+            if (ec.classList.contains('selected')) {
+                selectedElements.push(ec.textContent);
+            }
+            if (ec === e) {
+                idx = i;
+                selectedElements.push(ec.value);
+            }
+        });
+    }
+
+    const returnValue = {
+        key: e.id && e.id.charAt(0) !== '_' ? e.id : sdpiItem.id,
+        value: isList
+               ? e.textContent
+               : e.value === 'true'
+                 ? true 
+                 : e.value === 'false'
+                   ? false
+                   : e.value
+                     ? e.type === 'file'
+                       ? decodeURIComponent(e.value.replace(/^C:\\fakepath\\/, ''))
+                       : e.value
+                     : e.getAttribute('value'),
+        group: sdpiItemGroup ? sdpiItemGroup.id : false,
+        index: idx,
+        selection: selectedElements,
+        checked: e.checked
+    };
+
+    /** Just simulate the original file-selector:
+     * If there's an element of class '.sdpi-file-info'
+     * show the filename there
+     */
+    if (e.type === 'file') {
+        const info = sdpiItem.querySelector('.sdpi-file-info');
+        if (info) {
+            info.textContent = trimFileName(returnValue.value);
+        }
+    }
+
+    settings[returnValue.key] = returnValue.value;
+
+    if($SD && $SD.connection) {
+        console.log('setSettings(): ', settings);
+        $SD.api.setSettings($SD.uuid, settings);
+    }
+
+    showHideSettings();
+}
+
+function trimFileName(f) {
+    const s = f.split('/').pop();
+    const l = s.length;
+    return (l > 28) ? s.substr(0, 10) + '...' + s.substr(l - 10, l) : s;
+}
+
+function autoGrow(el) {
+    el.style.height = "auto";
+    el.style.height = (el.scrollHeight)+"px";
+}
+
+function showHideSettings() {
+    var d;
+    d = document.getElementById('response_parse_settings');
+    d.style.display = settings.response_parse ? "" : "none";
+
+    d = document.getElementById('poll_status_settings');
+    d.style.display = settings.poll_status ? "" : "none";
+
+    d = document.getElementById('poll_status_parse_settings');
+    d.style.display = settings.poll_status_parse ? "" : "none";
+}
+
+function localize(s) {
+    if(Utils.isUndefined(s)) return '';
+    let str = String(s);
+    try {
+        str = $localizedStrings[str] || str;
+    } catch(b) {}
+    return str;
+};
+
+// eslint-disable-next-line no-unused-vars
+function localizeUI () {
+    const el = document.querySelector('.sdpi-wrapper');
+    Array.from(el.querySelectorAll('.sdpi-item-label')).forEach(e => {
+        e.innerHTML = e.innerHTML.replace(e.innerText, localize(e.innerText));
+        console.log(`sdpi-label Localized ${getDomPath(e)}`, e);
+    });
+    Array.from(el.querySelectorAll('*:not(script)')).forEach(e => {
+        if (e.childNodes && e.childNodes.length > 0 && e.childNodes[0].nodeValue && typeof e.childNodes[0].nodeValue === 'string') {
+            e.childNodes[0].nodeValue = localize(e.childNodes[0].nodeValue);
+            console.log(`Localized ${getDomPath(e)}`, e);
+        }
+    });
+};
+
+function getDomPath(el) {
+    var stack = [];
+    while ( el.parentNode != null ) {
+      var sibCount = 0;
+      var sibIndex = 0;
+      for ( var i = 0; i < el.parentNode.childNodes.length; i++ ) {
+        var sib = el.parentNode.childNodes[i];
+        if ( sib.nodeName == el.nodeName ) {
+          if ( sib === el ) {
+            sibIndex = sibCount;
+          }
+          sibCount++;
+        }
+      }
+      if ( el.hasAttribute('id') && el.id != '' ) {
+        stack.unshift(el.nodeName.toLowerCase() + '#' + el.id);
+      } else if ( sibCount > 1 ) {
+        stack.unshift(el.nodeName.toLowerCase() + ':eq(' + sibIndex + ')');
+      } else {
+        stack.unshift(el.nodeName.toLowerCase());
+      }
+      el = el.parentNode;
+    }
+  
+    return stack.slice(1).join(' > '); // removes the html element
+}
